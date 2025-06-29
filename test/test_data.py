669303d40a16d6b10fa22cbb14b238b46207c354
naushin-7 <21f1007109@ds.study.iitm.ac.in>
2025-06-29 16:14:12 +0530

    Other required files

---
+++ test/test_data.py
@@ -0,0 +1,21 @@
+import unittest
+import pandas as pd
+import os, sys 
+
+class TestDataValidation(unittest.TestCase):
+    # Check data sape
+    def test_data_shape(self):
+        df = pd.read_csv(csv_path)
+        self.assertEqual(df.shape, (75, 5))
+
+    # Check for missing values
+    def test_no_missing_values(self):
+        df = pd.read_csv(csv_path)
+        self.assertFalse(df.isnull().values.any())
+
+if __name__ == '__main__':
+    pathname = os.path.dirname(os.path.dirname(sys.argv[0]))
+    path = os.path.abspath(pathname)
+    data_dir = os.path.join('test_data','iris.csv')
+    csv_path = os.path.join(path,data_dir)
+    unittest.main()
